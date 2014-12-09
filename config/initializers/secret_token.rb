78c770d54c9a2ebda640e391cffeda7545b1ea99
Frederik Lamote <frederiklamote@hotmail.com>
2014-12-09 20:50:55 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Romantify::Application.config.secret_key_base = '443d8f342773079ef630d18ad97fec39f666dd59fd36095a5bb9b89e7e226430d628afbf60d52c7def501c9f4151c7c4baee28557c27d8cef488d855c06c8f0f'
